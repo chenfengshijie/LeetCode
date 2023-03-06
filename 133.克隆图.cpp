e3857f426545f9f1c962a5733e3c15982baa2b1e
frozeworld <495964337@qq.com>
2023-03-06 19:46:21 +0800

    new file:   .vscode/extensions.json 	new file:   .vscode/settings.json 	new file:   "10.\346\255\243\345\210\231\350\241\250\350\276\276\345\274\217\345\214\271\351\205\215.cpp" 	new file:   "133.\345\205\213\351\232\206\345\233\276.cpp" 	new file:   "274.h-\346\214\207\346\225\260.cpp" 	new file:   "57.\346\217\222\345\205\245\345\214\272\351\227\264.cpp" 	new file:   Tmp.cpp 	new file:   Tmp.py 	new file:   input.txt

---
+++ 133.克隆图.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=133 lang=cpp
+ *
+ * [133] 克隆图
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.*/
+#include<vector>
+#include<queue>
+using std::vector;
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) 
+    {
+        
+    }
+private:
+    bool visited[109];
+    std::queue<int,vector<int>
+};
+// @lc code=end
+
