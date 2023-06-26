d75233aa36118ba0962ceaa95e137330bd9d4ba2
frozeworld <495964337@qq.com>
2023-06-26 22:40:38 +0800

    dance link

--- Tmp.py
+++ Tmp.py
@@ -1,3 +1,17 @@
-import re
+import os
 
-r1 = re.compile()
+
+def remove_empty_directories(directory):
+    for dir_name in os.listdir(directory):
+        dir_path = os.path.join(directory, dir_name)
+        try:
+            if os.path.isdir(dir_path) and not os.listdir(dir_path):  # 检查目录是否为空
+                os.rmdir(dir_path)  # 删除空目录
+        except (PermissionError, OSError) as e:
+            print("Failed to remove: " + dir_path)
+            continue
+
+
+# 使用示例
+directory_path = r"C:\Users\FrozeWorld"
+remove_empty_directories(directory_path)
